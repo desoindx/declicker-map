6f1d93beb873160b3fc5e40c853d0bef1c04590e
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2023-11-11 19:45:54 +0100

    revalidate every day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Map from '@/components/map/Map'
 import { getDeclikers } from '@/utils/getDeclikers'
 
+export const revalidate = 3600 * 24
+
 export default async function Home() {
   const declikers = await getDeclikers()
   return <Map declikers={declikers} />
